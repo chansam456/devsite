b7e7681376483b48a026b23765091ae23e81d0fc
Sam Chan <chansam55260@gmail.com>
2025-12-04 21:32:23 -0800

    Started baseline background framework

--- src/components/ShaderBackground.tsx
+++ src/components/ShaderBackground.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+
+// --- 1. Vertex Shader ----
+const vertexShader = '';
+
+// --- 2. Fragement Shader ---
+const fragmentShader = ''; //glsl code here
+
+function Moon(): React.ReactElement {
+    return(
+        <>
+        </>
+    );
+}
+
+function Stars(): React.ReactElement {
+    return(
+        <>
+        </>
+    );
+}
 
 export default function ShaderBackground(): React.ReactElement {
 
+    useFrame(() => ('something'));
+
     return(
-    <>
-    
-    </>
+    <Canvas>
+        <Moon />
+        <Stars />
+    </Canvas>
     );
 }
