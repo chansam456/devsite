5918f20ecff9ceff00d8ac992c27aedb1cafd212
Sam Chan <chansam55260@gmail.com>
2025-12-12 11:57:24 -0800

    Organized color palettes

---
+++ palettes.js
@@ -0,0 +1,17 @@
+export const lunar = {
+    background: '#0d1117',
+    backgroundshader: '#16202c',
+    accentblue: '#58a6ff',
+    accentorange: '#FD5C39',
+    textlight: '#e8e6e3', 
+    textdark: '#8b949e',
+    bright: '#fff',
+}
+
+export const solar = {
+    background: '#002b36',
+    accentpink: '#e8429c',
+    textlight: '#fdf8e8',
+    textdark: '#c4ba98',
+    bright: '#fff',
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { lunar, solar } from './palettes';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,25 +10,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        normal: { //lunar
-          background: '#0d1117',
-          backgroundshader: '#16202c',
-          accentblue: '#58a6ff',
-          accentorange: '#FD5C39',
-          textlight: '#e8e6e3', 
-          textdark: '#8b949e',
-          bright: '#fff',
-        },
-        
-        solar: {
-          background: '#002b36',
-          accentpink: '#e8429c',
-          textlight: '#fdf8e8',
-          textdark: '#c4ba98',
-          bright: '#fff',
-        }
+        normal: lunar,
+        solar: solar,
       },
-
       fontFamily: {
         main: ['"JetBrains Mono"', 'monospace']
       },
